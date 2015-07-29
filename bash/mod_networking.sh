ca13ebdbfc315b2206dd02b3733047cc507cbafe
Mike Mackintosh <mike@signalsciences.com>
2015-07-29 13:09:31 -0400

    add nscan function

--- bash/mod_networking.sh
+++ bash/mod_networking.sh
@@ -8,3 +8,8 @@ function nc {
 function lldp {
     sudo tcpdump -vvv -i any -c 1 ether proto 0x88cc
 }
+
+function nscan() {
+  echo "TCP-ACK Scanning target: $1"
+  sudo nmap -sA -Pn -p 20,21,23,25,80,443,1194,8443 $1
+}
