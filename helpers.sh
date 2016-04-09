ada20bed3c69633e1710d80e210cfc70f3c43360
Mike Mackintosh <m@zyp.io>
2016-04-08 23:04:35 -0400

    formatting needs love

--- helpers.sh
+++ helpers.sh
@@ -2,8 +2,13 @@
 
 # Helper function for outputting information
 
-# A log will provide a green indicator at the beggining of the output
+# A log will offset the start to match success and errors
 function log() {
+  echo -e "==> ${@}"
+}
+
+# A success will provide a green indicator at the beggining of the output
+function success() {
   echo -e "\033[0m[\033[38;5;2m*\033[0m] ${@}"
 }
 
