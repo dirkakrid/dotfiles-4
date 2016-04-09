07073b9eb9854a76a2b79dfa778ac13b16a631fb
Mike Mackintosh <m@zyp.io>
2016-04-08 23:22:27 -0400

    add git helpers to bash

---
+++ bash/git.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function git_branch {
+    local branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+
+    if [ -e $branch ]; then
+        return
+    else
+        if [ $branch == 'master' ]; then
+            COLOR="${SPLG_GREEN}"
+        elif [ $branch == 'development' ]; then
+            COLOR="${SPLG_ORANGE}"
+        elif [ $branch == 'dev' ]; then
+            COLOR="${SPLG_ORANGE}"
+        else
+            COLOR="${SPLG_PINK}"
+        fi
+
+        echo -e "${SPLG_LGREY}\302\261${COLOR} ${branch}"
+
+    fi
+}
+
+function branch {
+    git symbolic-ref --short HEAD
+}
+
+# Git
+alias pp='git pull --rebase && git push'
+alias gc='git clone'
+alias gs='git status'
+alias gl='git lg'
+alias commit='git commit -m'
