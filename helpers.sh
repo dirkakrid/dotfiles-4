6c24cc739096838f794dfc19753adef854cf1ea9
Mike Mackintosh <m@zyp.io>
2016-04-08 23:01:32 -0400

    add an error output

--- helpers.sh
+++ helpers.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 # Helper function for outputting information
+
+# A log will provide a green indicator at the beggining of the output
 function log() {
   echo -e "\033[0m[\033[38;5;2m*\033[0m] ${@}"
+}
+
+# Error will retun a red indication at the beggining of the output
+function error() {
+  echo -e "\033[0m[\033[38;5;1m*\033[0m] ${@}"
 }
