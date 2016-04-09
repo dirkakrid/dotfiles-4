90b8caecbe602a1a71f5e661b2c722707cd1ca12
Mike Mackintosh <m@zyp.io>
2016-04-08 23:23:48 -0400

    add go helpers to bash

---
+++ bash/go.sh
@@ -0,0 +1,32 @@
+# MOST AWESOMEST THING EVER
+function goto() {
+  echo -n "Switching to ${1}..."
+  TARGETS=($(find $GOPATH/src -name $1 -type d -maxdepth 3))
+  TARGET_COUNT=${#TARGETS[@]}
+  if [ $TARGET_COUNT -eq 0 ]; then
+    echo " Sorry! No matching dirs"
+    return
+  elif [ $TARGET_COUNT -eq 1 ]; then
+    cd "${TARGETS[0]}"
+  else
+    echo -e "\nWe found more than one location matching you input.\n"
+    for i in "${!TARGETS[@]}"; do
+      item=$(($i + 1))
+      echo "  $item: ${TARGETS[$i]}";
+    done
+
+    echo -en "\nPlease choose a destination: "
+    read idnum
+    itemnum=$(($idnum - 1))
+    echo "Switching to ${TARGETS[itemnum]}"
+    cd "${TARGETS[itemnum]}"
+  fi
+}
+
+_goto()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $( compgen -W "$(find $GOPATH/src -type d -maxdepth 3 -exec basename {} \;)" -- "$cur" ) )
+    return 0
+}
+complete -F _goto goto
