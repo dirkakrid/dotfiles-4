cc50be8bb2d48cd3e6cb2f36d17c882eb2efccf0
Mike Mackintosh <m@zyp.io>
2016-05-05 22:13:35 -0400

    add in some ip helpers. needs love

---
+++ bash/ip.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# ip.sh
+# -> returns the IP of the specified interface
+function mip() {
+
+  if [[ $1 == 'p' ]]; then
+    dig +short myip.opendns.com @resolver1.opendns.com
+    #curl icanhazip.com
+    return
+  fi
+
+  if [[ $1 == 't' ]]; then
+    curl icanhaztraceroute.com
+    return
+  fi
+
+  if [[ ! -z $1 ]]; then
+    ipconfig getifaddr $1
+    return
+  fi
+
+  IFACES=$(ifconfig -l)
+  for i in $IFACES; do
+    IP=$(ipconfig getifaddr $i)
+    if [[ ! -z $IP ]]; then
+      echo $(printf $SPLG_GREEN)$i$(printf $CLEAR): $IP
+    fi
+  done
+
+}
+
+function pub() {
+  mip p
+}
+
+_mip()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $( compgen -W "$(ifconfig -l)" -- "$cur" ) )
+    return 0
+}
+complete -F _mip mip
